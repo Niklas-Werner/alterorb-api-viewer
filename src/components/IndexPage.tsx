6246094ade592c37dc8219a1d65d694f81727cb8
Niklas Werner <Niklas-Werner@outlook.com>
2020-01-21 17:28:42 +0100

    index page content

--- src/components/IndexPage.tsx
+++ src/components/IndexPage.tsx
@@ -1,12 +1,37 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './IndexPage.scss';
 import { Layout } from './Layout';
 
 export default function IndexPage() {
     return (
         <Layout>
-            <p>
-                AlterOrb API Viewer
-            </p>
+            <ul className='box-link-list'>
+                <li>
+                    <Link to='/highscores'>
+                        <h3>Highscores</h3>
+                        <p>
+                            Top 10 players by orb points.
+                        </p>
+                    </Link>
+                </li>
+                <li>
+                    <Link to='/games'>
+                        <h3>Games</h3>
+                        <p>
+                            List of games and achievements.
+                        </p>
+                    </Link>
+                </li>
+                <li>
+                    <Link to='/players'>
+                        <h3>Players</h3>
+                        <p>
+                            Search for players.
+                        </p>
+                    </Link>
+                </li>
+            </ul>
         </Layout>
     );
 }
