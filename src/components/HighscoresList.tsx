8e97d2cc1733aacd33040098f0c87abb01591583
Niklas Werner <Niklas-Werner@outlook.com>
2020-01-21 21:50:48 +0100

    styled highscores and new font

--- src/components/HighscoresList.tsx
+++ src/components/HighscoresList.tsx
@@ -1,29 +1,35 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import { fetchHighscores } from '../store/data/actions';
-import { useActionCreatorEffect } from '../utils';
+import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom';
-import { getHighscores, getFetchingHighscores } from '../store/data/selectors';
+import './HighscoresList.scss';
 
-export function HighscoresList() {
-    const fetchingHighscores = useSelector(getFetchingHighscores);
-    const highscores = useSelector(getHighscores);
+function formatLargeInteger(num: number) {
+    const str = num.toFixed();
+    let result = [];
+    for (let i = str.length - 1; i >= 0; i -= 3)
+        result.unshift(str.substring(Math.max(0, i - 3), i));
+    return result.join('\u202f');
+}
 
-    useActionCreatorEffect(fetchHighscores);
+export function HighscoresList(props: {
+    title: string;
+    scoreLabel: string;
+    highscores: [string, number][];
+}) {
+    const { title, scoreLabel, highscores } = props;
 
-    if (fetchingHighscores)
-        return <p>Fetching highscores list...</p>;
-
-    if (!highscores)
-        return <p>No highscores found.</p>;
-
-    return <>
-        <ol>
-            {highscores.map((entry, index) =>
-                <li key={index}>
-                    <Link to={`/players/${entry.displayName}`}>{entry.displayName}</Link>: {entry.orbPoints}P
-            </li>
-            )}
-        </ol>
-    </>;
+    return (
+        <div className='highscores-list'>
+            <h3>{title}</h3>
+            <div className='table'>
+                <span className='head rank'>Rank</span>
+                <span className='head name'>Name</span>
+                <span className='head score'>{scoreLabel}</span>
+                {highscores.map(([name, score], index) => <Fragment key={index}>
+                    <span className='rank'>{index + 1}</span>
+                    <span className='name'><Link to={`/players/${name}`}>{name}</Link></span>
+                    <span className='score'>{formatLargeInteger(score)}</span>
+                </Fragment>)}
+            </div>
+        </div>
+    );
 }
--- src/pages/HighscoresPage.tsx
+++ src/pages/HighscoresPage.tsx
@@ -1,11 +1,38 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { createSelector } from 'reselect';
 import { HighscoresList } from '../components/HighscoresList';
 import { Layout } from '../components/layout/Layout';
+import { fetchHighscores } from '../store/data/actions';
+import { getFetchingHighscores, getHighscores } from '../store/data/selectors';
+import { useActionCreatorEffect } from '../utils';
+
+const getHighscoresListEntries = createSelector(
+    [getHighscores],
+    (highscores) => {
+        if (!highscores)
+            return null;
+        return highscores.map<[string, number]>(entry => [
+            entry.displayName!,
+            entry.orbPoints!
+        ]);
+    }
+)
 
 export function HighscoresPage() {
+    const fetchingHighscores = useSelector(getFetchingHighscores);
+    const highscoresListEntries = useSelector(getHighscoresListEntries);
+
+    useActionCreatorEffect(fetchHighscores);
+
     return (
         <Layout title='Highscores'>
-            <HighscoresList />
+            {fetchingHighscores &&
+                <p>Fetching highscores list...</p>
+            }
+            {highscoresListEntries &&
+                <HighscoresList title='Top 10 Orb Points' scoreLabel='Orb Points' highscores={highscoresListEntries} />
+            }
         </Layout>
     );
 }
